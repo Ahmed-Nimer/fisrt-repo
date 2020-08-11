aed8f6d6d5db7ba2ea2371d96ef5e9f62a77297e
Ahmed-Nimer <ahmed.nimer9995@gmail.com>
2020-08-11 15:37:09 +0300

    add contact form and google map

--- main.js
+++ main.js
@@ -37,3 +37,16 @@ $('.cf a').on('click', function(event) {
     );
   }
 });
+
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 15.653120, lng: 32.481529 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
